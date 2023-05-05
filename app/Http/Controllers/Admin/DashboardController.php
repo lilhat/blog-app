34f01f789d162a719d1778c9cfe5fb486c9b6b26
lilhat <tah.fighter@hotmail.co.uk>
2023-05-05 01:23:22 +0100

    dashboard to show statistics

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\BlogPost;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $categories = Category::count();
+        $posts = BlogPost::count();
+        $users = User::where('role_as','0')->count();
+        $admins = User::where('role_as','1')->count();
+
+        return view('admin.dashboard', compact('categories','posts','users','admins'));
     }
 }
